7f980f8cae653e3a25c39a11229d6f397cc76e4f
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2022-12-05 19:26:12 +0900

    Create day_two.py

---
+++ day_two.py
@@ -0,0 +1,11 @@
+#Challenge #2: Strings to Integers
+def convert_add(nums):
+    return sum(map(int,nums))
+
+#Extra Challenge: Duplicate Names
+def check_duplicates(arr):
+    return ', '.join({i for i in arr if arr.count(i)>1}) or "No duplicates"
+
+print(convert_add(['1','20','3']))
+print(check_duplicates(['apple', 'orange', 'banana', 'apple', 'orange']))
+print(check_duplicates(['Yoda', 'Moses', 'Joshua', 'Mark']))
