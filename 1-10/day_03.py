7bbb0084f0f95aaf50fe79a227dc0662d7774e81
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2022-12-06 16:02:25 +0900

    Update day_03.py

--- 1-10/day_03.py
+++ 1-10/day_03.py
@@ -1,11 +1,17 @@
-#Challenge #: TITLE
-def :
-    return 
+#Challenge 3: Register Check
+def register_check(reg):
+    return [x.upper() for x in reg.values()].count('YES')
 
-#Extra Challenge: TITLE
-def :
-    return 
+#Extra Challenge: Lowercase Names
+def lower_names(names):
+    return tuple(n for n in sorted(names,reverse=True) if n == n.lower())
+    
 
-print()
-print()
-print()
+
+register = {'Michael': 'yes', 'John': 'no', 'Peter': 'Yes', 'Mary': 'Yes'}
+print(register_check(register))
+
+names = ['kerry','#','John','Mary','carol','Rose','adam']
+print(lower_names(names))
+names = ['alice','BETTY','Joan','xarlos']
+print(lower_names(names))
