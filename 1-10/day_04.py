a0bb69e63834aca8ac77f72b9fe1d41534d85b5d
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2022-12-07 15:57:47 +0900

    Update day_04.py

--- 1-10/day_04.py
+++ 1-10/day_04.py
@@ -1,11 +1,19 @@
-#Challenge #: TITLE
-def :
-    return 
+#Challenge 4: Only Floats
+def only_floats(a, b):
+    return sum(isinstance(i, float) for i in (a,b))
 
-#Extra Challenge: TITLE
-def :
-    return 
+#Extra Challenge: Index of the Longest Word
+def word_index(words):
+    """for Xarlos:
+    return 0 if len((s:=sorted(words,key=len,reverse=True))[0]) == len(s[1]) else words.index(s[0])
+    """
+    s_words = sorted(words,key=len,reverse=True)
+    long_i = words.index(s_words[0])
+    return 0 if len(s_words[0]) == len(s_words[1]) else long_i
 
-print()
-print()
-print()
+print(only_floats(12.1,23))
+print(only_floats(12.5,23.6))
+print(only_floats(12,23))
+
+print(word_index(["Hate", "remorse", "vengeance", "ire"]))
+print(word_index(["Love", "Hate"]))
