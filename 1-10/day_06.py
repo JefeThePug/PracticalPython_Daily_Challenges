82b012dc51445bf32dbcf4e6d974a4e2b646d1ac
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2022-12-09 18:51:12 +0900

    Update day_06.py

--- 1-10/day_06.py
+++ 1-10/day_06.py
@@ -1,11 +1,11 @@
-#Challenge #: TITLE
-def :
-    return 
+#Challenge 6: User Name Generator
+def user_name():
+    return input("Enter your email: ").split("@")[0]
 
-#Extra Challenge: TITLE
-def :
-    return 
+#Extra Challenge: Zero Both Ends
+def zeroed_code(nums):
+    return [0,*nums[1:-1],0]
 
-print()
-print()
-print()
+print(user_name())
+
+print(zeroed_code([2, 5, 7, 8, 9]))
