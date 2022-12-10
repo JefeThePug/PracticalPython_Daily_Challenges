dcb46e011c70a0eb8749a36be3ca25b1e773d67c
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2022-12-10 18:17:52 +0900

    Update day_07.py

--- 1-10/day_07.py
+++ 1-10/day_07.py
@@ -1,11 +1,15 @@
-#Challenge #: TITLE
-def :
-    return 
+#Challenge 7: A String Range
+def string_range(num):
+    return ".".join(map(str,range(num)))
 
-#Extra Challenge: TITLE
-def :
-    return 
+#Extra Challenge: Dictionary of Names
+def s_names(names):
+    return {i:names.count(i) for i in {n for n in names if n[0] == "S"}}
 
-print()
-print()
-print()
+print(string_range(6))
+print(string_range(20))
+
+print(s_names(["Joseph","Nathan", "Sasha", "Kelly", "Muhammad", "Jabulani", "Sera", "Patel", "Sera"]))
+print(s_names(["Sally","Sally", "Sally", "Sally", "Salli", "Jabulani", "Sera", "Patel", "Sera"]))
+print(s_names(["Anne", "Peter", "Bob", "Paul"]))
+print(s_names(["Steven"]))
