c55e941c5b4c26395deda1ecc6a4f2696d31f18b
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2022-12-14 17:23:00 +0900

    Update day_11.py

--- 11-20/day_11.py
+++ 11-20/day_11.py
@@ -1,11 +1,15 @@
-#Challenge #: TITLE
-def :
-    return 
+#Challenge 11: Are They Equal?
+def equal_strings(str1, str2):
+    return sorted(str1.lower()) == sorted(str2.lower())
 
-#Extra Challenge: TITLE
-def :
-    return 
+#Extra Challenge: Swap Values
+def swap_values(arr):
+    if not arr: return []
+    arr[0], arr[-1] = arr[-1], arr[0]
+    return arr
 
-print()
-print()
-print()
+print(equal_strings("love", "evol"))
+print(equal_strings("Live", "eVIL"))
+
+print(swap_values([2,4,67,7]))
+print(swap_values([9]))
