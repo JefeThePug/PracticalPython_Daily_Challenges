4b3f889c23d8fbc0eca7e7ee269ede17bc3a5ba1
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2022-12-15 19:50:08 +0900

    Update day_12.py

--- 11-20/day_12.py
+++ 11-20/day_12.py
@@ -1,11 +1,26 @@
-#Challenge #: TITLE
-def :
-    return 
+#Challenge 12: Count the Dots
+def count_dots(s):
+    return s.count('.')
 
-#Extra Challenge: TITLE
-def :
-    return 
+#Extra Challenge: Your Age in Minutes
+def get_year(this_year):
+    while True:
+        year = input("Enter your four-digit birth year (eg 1988): ")
+        if not year.isdigit(): 
+            print("Try again with numbers.")
+        elif 1900 <= int(year) <= this_year:
+            return int(year)
+        else:
+            print("Input a valid year.")
 
-print()
-print()
-print()
+def age_in_minutes():
+    from datetime import date
+    this_year = date.today().year
+    birth_year = get_year(this_year)
+    delta_secs = (date(this_year,1,1) - date(birth_year,1,1)).total_seconds()
+    return int(delta_secs/60)
+
+print(count_dots('h.e.l.p.'))
+print(count_dots('he.lp.'))
+
+print(age_in_minutes())
