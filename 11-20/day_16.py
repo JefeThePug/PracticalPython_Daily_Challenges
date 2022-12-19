5b7e0e0a8248148422fc59d30bea42d8c5ee5ae7
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2022-12-19 23:05:03 +0900

    Update day_16.py

--- 11-20/day_16.py
+++ 11-20/day_16.py
@@ -1,11 +1,11 @@
-#Challenge #: TITLE
-def :
-    return 
+#Challenge 16: Sum the List
+def sum_list(arr):
+    return sum([sum(i) for i in arr])
 
-#Extra Challenge: TITLE
-def :
-    return 
+#Extra Challenge: Unpack A Nest
+def unpack(arr):
+    return [*{i for a in arr for i in a if i in (34,67,78)}]
 
-print()
-print()
-print()
+print(sum_list([[2,4,5,6],[2,3,5,6]]))
+
+print(unpack([[12, 34, 56, 67], [34, 68, 78]]))
