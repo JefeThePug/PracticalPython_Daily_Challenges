3e92b906b52776a30df7d04b698bcc61a0decd55
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2022-12-20 17:33:31 +0900

    Update day_17.py

--- 11-20/day_17.py
+++ 11-20/day_17.py
@@ -1,11 +1,23 @@
-#Challenge #: TITLE
-def :
-    return 
+#Challenge 17: User Name Generator
 
-#Extra Challenge: TITLE
-def :
-    return 
+from random import randint
 
-print()
-print()
-print()
+def user_name(name):
+    return f'{name[::-1].lower()}{randint(0,9)}'
+
+#Extra Challenge: Sort by Length
+
+def sort_length(names):
+    # boo why not use built-in sort?? >>> return sorted(names, key=len)
+    ref = {}
+    for name in names:
+        ref.setdefault(len(name), []).append(name)
+    output = [ref[i] for i in range(max(ref.keys())+1) if i in ref]
+    return sum(output, []) #flattens the list of lists
+                   
+
+print(user_name('chiaki'))
+print(user_name('Xarlos'))
+
+print(sort_length( ['Peter','Jon','Andrew']))
+print(sort_length( ['Peter','Jon','Frank','Andrew']))
