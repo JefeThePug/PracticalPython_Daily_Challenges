a1ba862c25256f9a6ed51998a5167e94f0c90f92
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2022-12-21 18:41:27 +0900

    Update day_18.py

--- 11-20/day_18.py
+++ 11-20/day_18.py
@@ -1,11 +1,16 @@
-#Challenge #: TITLE
-def :
-    return 
+#Challenge 18: Any Number of Arguments
+def any_number(*args):
+    return sum(args)/len(args)
 
-#Extra Challenge: TITLE
-def :
-    return 
+#Extra Challenge: Add and Reverse
+def add_reverse(a, b):
+    if len(a) != len(b): 
+        return "the lists are not of equal lengths"
+    return [*map(sum,zip(a,b))][::-1]
 
-print()
-print()
-print()
+print(any_number(12,90,12,34))
+print(any_number(12,90))
+print(any_number(12.33,1))
+
+print(add_reverse([10, 12, 34],[12, 56, 78]))
+print(add_reverse([1],[90,100,55]))
