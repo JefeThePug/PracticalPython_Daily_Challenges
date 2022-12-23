39a841dd62c70351cb699cd2c56e6c81d59f12a7
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2022-12-23 18:40:17 +0900

    Update day_20.py

--- 11-20/day_20.py
+++ 11-20/day_20.py
@@ -1,11 +1,18 @@
-#Challenge #: TITLE
-def :
-    return 
+#Challenge 20: Capitalize First Letter
+def capitalized(s):
+    return s.title()
 
-#Extra Challenge: TITLE
-def :
-    return 
+#Extra Challenge: Reversed List
+import re
 
-print()
-print()
-print()
+def reversed_list(s):
+    pattern = r'\w*[A-Z]\w*'
+    return [i[::-1] for i in re.findall(pattern, s)]
+
+print(capitalized('i like learning'))
+print(capitalized("i love cheese"))
+
+str1 = 'leArning is hard, bUt if You appLy youRself you can achieVe success'
+print(reversed_list(str1))
+str1 = 'this will give you nothing'
+print(reversed_list(str1))
