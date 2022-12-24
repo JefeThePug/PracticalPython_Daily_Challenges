91719a891a4c4474c27c6c4d745d9870a5914a71
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2022-12-25 00:47:05 +0900

    Update day_21.py

--- 21-30/day_21.py
+++ 21-30/day_21.py
@@ -1,11 +1,13 @@
-#Challenge #: TITLE
-def :
-    return 
+#Challenge 21: List of Tuples
+def make_tuples(a, b):
+    return list(zip(a,b))
 
-#Extra Challenge: TITLE
-def :
-    return 
+#Extra Challenge: Even Number or Average
+def even_or_average(nums):
+    evens = [n for n in nums if not n&1]
+    return max(evens) if evens else sum(nums)/len(nums)
 
-print()
-print()
-print()
+print(make_tuples([1,2,3,4],[5,6,7,8]))
+
+print(even_or_average([1,2,3,4,5]))
+print(even_or_average([1,3,5,7,9]))
