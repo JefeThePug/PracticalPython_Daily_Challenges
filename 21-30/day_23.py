8bc2fd6ce7feada8a0c89901ccc2e056ec488625
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2022-12-26 18:33:05 +0900

    Update day_23.py

--- 21-30/day_23.py
+++ 21-30/day_23.py
@@ -1,11 +1,29 @@
-#Challenge #: TITLE
-def :
-    return 
+#Challenge 23: Simple Calculator
+import re
 
-#Extra Challenge: TITLE
-def :
-    return 
+def get_equation():
+    txt = '''Please input a simple maths equation.
+      You can use +, -, ×(*), or ÷(/):'''
+    return input(txt).replace('×','*').replace('÷','/').rstrip('=')
 
-print()
-print()
-print()
+def calculate():
+    equation = get_equation()
+    while not re.match(r'^\d+[-+*/]\d+$', equation):
+        print("That is not a simple equation. Try again.")
+        equation = get_equation()
+    try:
+        result = eval(equation)
+    except ZeroDivisionError:
+        return "You cannot divide by zero!"
+    else:
+        return result 
+
+#Extra Challenge: Multiply Words
+from math import prod
+
+def multiply_words(s):
+    return prod([len(c) for c in s.split() if c.islower()])
+
+print(calculate())
+print(multiply_words('love live and laugh'))
+print(multiply_words('Hate war love Peace'))
