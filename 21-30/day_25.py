f0137c9601e2ba31fa0cb745a89b09d8ccf486fe
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2022-12-28 17:19:29 +0900

    Update day_25.py

--- 21-30/day_25.py
+++ 21-30/day_25.py
@@ -1,11 +1,15 @@
-#Challenge #: TITLE
-def :
-    return 
+#Challenge 25: All the Same
+def all_the_same(arr):
+    return len(set(arr)) == 1
 
-#Extra Challenge: TITLE
-def :
-    return 
+#Extra Challenge: Reverse a String
+def read_backwards(s):
+    return " ".join(s.split()[::-1])
 
-print()
-print()
-print()
+print(all_the_same(['Mary','Mary','Mary']))
+print(all_the_same(['Mary','mary','Mary']))
+
+str1 = 'the love is real'
+print(read_backwards(str1))
+str1 = "we'll train the train well"
+print(read_backwards(str1))
