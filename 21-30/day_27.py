6e97c8ff06b5cfd5a67e88a58a48892121cb74de
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2022-12-30 18:34:03 +0900

    Update day_27.py

--- 21-30/day_27.py
+++ 21-30/day_27.py
@@ -1,11 +1,16 @@
-#Challenge #: TITLE
-def :
-    return 
+#Challenge 27: Unique Numbers
+def unique_numbers(s):
+    if (sum(s) - sum(u:=set(s)))&1:
+        return [*u]
+    return s
 
-#Extra Challenge: TITLE
-def :
-    return 
+#Extra Challenge: Difference of Two Lists
+def difference_of_two(a,b):
+    return [n for n in a+b if (a+b).count(n)==1]
 
-print()
-print()
-print()
+print(unique_numbers([1,2,4,5,6,7,8,8]))
+print(unique_numbers([1,2,5,5,6,7,8,8]))
+
+a=[1,2,4,5,6]
+b=[1,2,5,7,9]
+print(difference_of_two(a,b))
