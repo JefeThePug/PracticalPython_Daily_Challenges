b37d466b103b92cfe879ced539ff201996a6c455
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2022-12-31 19:33:22 +0900

    Update day_28.py

--- 21-30/day_28.py
+++ 21-30/day_28.py
@@ -1,11 +1,14 @@
-#Challenge #: TITLE
-def :
-    return 
+def index_position(s):
+    return [i for i,c in enumerate(s) if c.islower()]
 
-#Extra Challenge: TITLE
-def :
-    return 
+#Extra Challenge: Largest Number
+def largest_number(nums):
+    return int("".join(sorted("".join(map(str,nums))))[::-1])
 
-print()
-print()
-print()
+print(index_position('LovE'))
+print(index_position('LOVE'))
+
+list1 = [3, 67, 87, 9, 2]
+print(largest_number(list1))
+list1 = [9991,992,94,93]
+print(largest_number(list1))
