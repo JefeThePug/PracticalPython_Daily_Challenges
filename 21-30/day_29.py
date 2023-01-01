a69357ee2238e5fe5200b276763e263b21edf02e
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2023-01-01 23:27:55 +0900

    Update day_29.py

--- 21-30/day_29.py
+++ 21-30/day_29.py
@@ -1,11 +1,23 @@
-#Challenge #: TITLE
-def :
-    return 
+#Challenge 29: Middle Figure
+from itertools import filterfalse as ff
+def middle_figure(a, b):
+    s = [*ff(str.isspace, a+b)]
+    if len(s)&1:
+        return s[len(s)//2]
+    return 'no middle figure'
 
-#Extra Challenge: TITLE
-def :
-    return 
+'''
+Shorter but less readable:
+def middle_figure(a, b):
+    s = [*ff(str.isspace, a+b)]
+    return s[len(s)//2]*(len(s)&1) or 'no middle figure'
+'''
 
-print()
-print()
-print()
+txt1='make love'
+txt2='not wars '
+print(middle_figure(txt1, txt2))
+txt2='not war'
+print(middle_figure(txt1, txt2))
+txt1='a'
+txt2=''
+print(middle_figure(txt1, txt2))
