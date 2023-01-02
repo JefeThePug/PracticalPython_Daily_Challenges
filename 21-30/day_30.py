4bea0083500f734c19cc0894018375c68e96bcd2
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2023-01-02 20:29:12 +0900

    Update day_30.py

--- 21-30/day_30.py
+++ 21-30/day_30.py
@@ -1,11 +1,21 @@
-#Challenge #: TITLE
-def :
-    return 
+#Challenge 30: Most Repeated Name
+from collections import Counter
+def repeated_name(arr):
+    return Counter(arr).most_common()[0][0]
 
-#Extra Challenge: TITLE
-def :
-    return 
+"""
+without Counter:
+def repeated_name(arr):
+    counts = [(n,arr.count(n)) for n in set(arr)]
+    return max(counts, key=lambda x:x[1])[0]
+"""
 
-print()
-print()
-print()
+#Extra Challenge: Sort by Last Name
+def swap_items(arr):
+    return sorted(f'{b} {a}'for a,b in map(str.split, arr))
+
+names = ['John','Peter','John','Peter','Jones','Peter']
+print(repeated_name(names))
+
+names = ['Beyonce Knowles','Alicia Keys','Katie Perry','Chris Brown','Tom Cruise']
+print(swap_items(names))
