9ab8baec6133070c3d988b02a03997ce9b6094d5
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2023-01-03 22:55:32 +0900

    Update day_31.py

--- 31-40/day_31.py
+++ 31-40/day_31.py
@@ -1,11 +1,20 @@
-#Challenge #: TITLE
-def :
-    return 
+#Challenge 31: Longest Word
+def longest_word(words):
+    return max([[len(w),w] for w in words], key=lambda x: len(x[1]))
 
 #Extra Challenge: TITLE
-def :
-    return 
+def create_user():
+    a,b,c = ['Input your first name: ', 'Input your age: ', 'Input your desired password: ']
+    user = {'name': input(a), 'age': input(b), 'password': input(c)}
+    print('User saved. Please login')
+    
+    while True:
+        correct_input = all([input('Enter your first name: ') == user['name'],
+                        input('Enter your password: ') == user['password']])
+        if correct_input:
+            return 'Logged in successfully'
+        print('Wrong password or user name. Please try again')
 
-print()
-print()
-print()
+print(longest_word(['Java', 'JavaScript', 'Python']))
+
+print(create_user())
