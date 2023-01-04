5badb14596ea5cc8cb957a964dbe814e71983811
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2023-01-04 17:32:10 +0900

    Update day_32.py

--- 31-40/day_32.py
+++ 31-40/day_32.py
@@ -1,11 +1,30 @@
-#Challenge #: TITLE
-def :
-    return 
+#Challenge 32: Password Validator
+def password_validator():
+    while True:
+        issues = ''
+        pwd = input('Enter a password: ')
+        if not any(c.isupper() for c in pwd):
+            issues += 'You must include at least one uppercase letter.\n'
+        if not any(c.islower() for c in pwd):
+            issues += 'You must include at least one lowercase letter.\n'
+        if not any(c.isdigit() for c in pwd):
+            issues += 'You must include at least one number.\n'
+        if len(pwd) < 7:
+            issues += 'Your password must be at least 8 characters long.\n'
+        if not issues:
+            return pwd
+        print(issues, 'Please enter a valid password.', sep="")
 
-#Extra Challenge: TITLE
-def :
-    return 
 
-print()
-print()
-print()
+#Extra Challenge: Valid Email
+import re
+
+def email_validator(emails):
+    matches = [e for e in emails if re.match(r'\w+@\w+\.com', e)] 
+    return matches or 'all emails are invalid'
+
+print(password_validator())
+emails = ['ben@mail.com', 'john@mail.cm', 'kenny@gmail.com', '@list.com']
+print(email_validator(emails))
+emails = ['b@en@mail.com', 'john@mail.cm', 'kenny@.com', '@list.com']
+print(email_validator(emails))
