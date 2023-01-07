8afcfc6d8a3c5ee7db80d0161166087bc8db0ab6
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2023-01-07 18:28:06 +0900

    Update day_35.py

--- 31-40/day_35.py
+++ 31-40/day_35.py
@@ -1,11 +1,15 @@
-#Challenge #: TITLE
-def :
-    return 
+#Challenge 35: Pangram
+from string import ascii_lowercase
 
-#Extra Challenge: TITLE
-def :
-    return 
+def check_pangram(s):
+    return not set(ascii_lowercase).difference(s)
 
-print()
-print()
-print()
+#Extra Challenge: Find my Position
+def find_index(arr, i):
+    return [x for x,n in enumerate(arr) if n == i] or [i]*len(arr)
+
+print(check_pangram('the quick brown fox jumps over a lazy dog'))
+print(check_pangram('Mary had a little lamb whose fleece was white as snow'))
+
+print(find_index([1, 2, 4, 6, 7, 7], 7))
+print(find_index([1, 2, 4, 6, 7, 7], 8))
