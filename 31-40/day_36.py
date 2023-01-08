37e8b50cfd560b027251df0c55488e65e0a8a3a9
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2023-01-08 19:23:11 +0900

    Update day_36.py

--- 31-40/day_36.py
+++ 31-40/day_36.py
@@ -1,11 +1,8 @@
-#Challenge #: TITLE
-def :
-    return 
+#Challenge 36: Count String
+def count_str(s):
+    return {c:s.count(c) for c in s.lower()}
 
-#Extra Challenge: TITLE
-def :
-    return 
-
-print()
-print()
-print()
+print(count_str('hello'))
+long_s = """'You can do anything you want to do as long as you keep a good attitude and keep working at it. 
+But the second you give up, you’re screwed.' Dolly Parton"""
+print(count_str(long_s))
