a0e78f4eeaa14beea5c48ca0ddf716986b9ac370
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2023-01-09 17:21:49 +0900

    Update day_37.py

--- 31-40/day_37.py
+++ 31-40/day_37.py
@@ -1,11 +1,21 @@
-#Challenge #: TITLE
-def :
-    return 
+#Challenge 37: Count the Vowels
 
-#Extra Challenge: TITLE
-def :
-    return 
+#if lowercase vowels and uppercase vowels should be considered different...
+def count_the_vowels_a(s):
+    return len(set('AEIOUaeiou').intersection(s))
 
-print()
-print()
-print()
+#if lowercase vowels and uppercase vowels should be considered the same...
+def count_the_vowels_b(s):
+    return len(set('aeiou').intersection(s.lower()))
+
+print(count_the_vowels_a('saas')) # >>>1
+print(count_the_vowels_a('sous')) # >>>2
+print(count_the_vowels_a('sAas')) # >>>2
+print(count_the_vowels_a('abcdefghijklmnopqrstuvwxyz')) # >>>5
+print(count_the_vowels_a('AEIOUaeiou')) # >>>10
+
+print(count_the_vowels_b('saas')) # >>>1
+print(count_the_vowels_b('sous')) # >>>2
+print(count_the_vowels_b('sAas')) # >>>1
+print(count_the_vowels_b('abcdefghijklmnopqrstuvwxyz')) # >>>5
+print(count_the_vowels_b('AEIOUaeiou')) # >>>5
