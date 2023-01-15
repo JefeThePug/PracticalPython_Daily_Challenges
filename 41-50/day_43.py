7bd29591b39903d997cfc064ded0c05c4360f630
JefeThePug <rob.mihalko@gmail.com>
2023-01-15 19:07:58 +0900

    Update day_43.py

--- 41-50/day_43.py
+++ 41-50/day_43.py
@@ -1,11 +1,10 @@
-#Challenge #: TITLE
-def :
-    return 
+#Challenge 43: Student Marks
+def student_marks():
+    marks = {}
+    while True:
+        name = input("Input student's name or 'done' to stop: ")
+        if name.lower().replace("'","").strip() == 'done': break
+        marks[name] = [*map(float,input(f"Input {name}'s marks, separated by spaces: ").split())]
+    return marks
 
-#Extra Challenge: TITLE
-def :
-    return 
-
-print()
-print()
-print()
+print(student_marks())
