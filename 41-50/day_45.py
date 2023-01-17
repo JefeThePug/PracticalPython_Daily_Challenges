a5847ba59a5d0e3e2e15ff5902105c465e51059c
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2023-01-17 19:33:57 +0900

    Update day_45.py

--- 41-50/day_45.py
+++ 41-50/day_45.py
@@ -1,11 +1,25 @@
-#Challenge #: TITLE
-def :
-    return 
+#Challenge 45: Words & Special Characters
+from string import punctuation, whitespace
+def analyse_string(s):
+    s = s.replace('.', '')
+    special = len([i for i in s if i in punctuation])
+    words = len([i for i in s.translate(s.maketrans('','',punctuation)).split() if not i.isnumeric()])
+    total = len([i for i in s if i not in whitespace])
+    return {'special character':special,'words':words,'total characters':total}
+         
 
-#Extra Challenge: TITLE
-def :
-    return 
+s= 'Python has a string format operator %. This functions analogously to printf format strings in C, e.g. "spam=%s eggs=%d" % ("blah", 2) evaluates to "spam=blah eggs=2".'
+print(analyse_string(s))
 
-print()
-print()
-print()
+_ = '''
+This is without a doubt the worst challenge yet.  
+Totally unclear about what constitutes a word 
+especially with an example sentence that's packed 
+with mathematical expressions and programming 
+that include words in various forms.
+Is spam=%s one word?  or is spam and s two words?
+Is C (the programming language) a word? 
+Is printf a word even though it is not actually a word?
+What about e.g.?  This was frustrating and I do not 
+care if the results are incorrect.  I made my own rules.
+'''
