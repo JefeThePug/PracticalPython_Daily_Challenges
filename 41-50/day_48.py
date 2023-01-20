d83d30a3cb55edac6af53e26c0f47c173135f257
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2023-01-20 18:30:01 +0900

    Update day_48.py

--- 41-50/day_48.py
+++ 41-50/day_48.py
@@ -1,11 +1,18 @@
-#Challenge #: TITLE
-def :
-    return 
+#Challenge 48: Binary Search
+def binary_search(arr, n):
+    arr.sort()
+    s, e = 0, len(arr)-1
+    while s <= e:
+        mid = (s+e)//2
+        if arr[mid] == n: 
+            return mid
+        elif arr[mid] < n:
+            s = mid + 1
+        elif arr[mid] > n:
+            e = mid - 1
+    return -1 #not in list
 
-#Extra Challenge: TITLE
-def :
-    return 
 
-print()
-print()
-print()
+list1 = [12, 34, 56, 78, 98, 22, 45, 13]
+print(binary_search(list1, 22))
+print(binary_search(list1, 15))
